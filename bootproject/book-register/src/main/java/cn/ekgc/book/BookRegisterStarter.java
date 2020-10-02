ae2f52ed7f14c9cee5091824e36d230de6a40743
小白 <1031689260@qq.com>
2020-10-02 12:02:29 +0800

    图书管理系统

---
+++ bootproject/book-register/src/main/java/cn/ekgc/book/BookRegisterStarter.java
@@ -0,0 +1,14 @@
+package cn.ekgc.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class BookRegisterStarter {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookRegisterStarter.class, args);
+	}
+}
